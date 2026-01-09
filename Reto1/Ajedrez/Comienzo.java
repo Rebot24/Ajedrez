20c1cf1a37620fc91c5bfb536682b00511d85bf5
sryounes <youunes.mr@gmail.com>
2026-01-09 08:58:32 +0100

    Add files via upload

--- Reto1/Ajedrez/Comienzo.java
+++ Reto1/Ajedrez/Comienzo.java
@@ -6,22 +6,22 @@ public class Comienzo {
         Scanner scanner = new Scanner(System.in);
         int opcion;
 
-        System.out.println("¿Quién empieza?");
+        System.out.println("¿Quien empieza?");
         System.out.println("1. Jugador");
-        System.out.println("2. Máquina");
-        System.out.print("Elige una opción (1 o 2): ");
+        System.out.println("2. Maquina");
+        System.out.print("Elige una opcion (1 o 2): ");
 
         opcion = scanner.nextInt();
 
         while (opcion != 1 && opcion != 2) {
-            System.out.print("Opción inválida. Elige 1 o 2: ");
+            System.out.print("Opcion invalida. Elige 1 o 2: ");
             opcion = scanner.nextInt();
         }
 
         if (opcion == 1) {
             System.out.println("Empieza el jugador.");
         } else {
-            System.out.println("Empieza la máquina.");
+            System.out.println("Empieza la maquina.");
         }
         scanner.close();
     }
